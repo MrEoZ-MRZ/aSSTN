bcb596bba85cccad4567df32aa6d024a915ed308
Nazareno Busi <Nazareno Busi@DESKTOP-GQJPLHP>
2021-10-05 20:38:03 -0300

    Update

--- app/src/main/java/com/mrz/asstn/MainActivity.java
+++ app/src/main/java/com/mrz/asstn/MainActivity.java
@@ -5,21 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button Escanear;
+    private StringBuilder Presentes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        IntentIntegrator integrator = new IntentIntegrator(this);
-        integrator.setPrompt("Escanea el documento");
-        Intent intent = integrator.createScanIntent();
-        startActivityForResult(intent, 120);
+        Escanear = findViewById(R.id.startbtn);
+        Escanear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.setPrompt("Escanea el documento");
+                Intent intent = integrator.createScanIntent();
+                startActivityForResult(intent, 120);
+            }
+        });
     }
 
     @Override
