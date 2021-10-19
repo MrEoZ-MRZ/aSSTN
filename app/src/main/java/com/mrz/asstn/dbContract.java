e27d73b0ad4e4c8c9f477b3d6a095b8e6a804222
Sofia Marzo <SofiMarzo@gmail.com>
2021-10-19 11:19:53 -0300

    Suma de base de datos

---
+++ app/src/main/java/com/mrz/asstn/dbContract.java
@@ -0,0 +1,13 @@
+package com.mrz.asstn;
+
+public class dbContract {
+    private String DB_NAME = "aSSTN";
+    private String TABLE_NAME = "322";
+    private String TABLE_6B = "6B";
+    private String TABLE_ALUMNOS = "Alumnos";
+    private String COLUM_NT = "Tramite";
+    private String COLUM_NA = "Datos";
+    private String COLUM_DNI = "DNI";
+    private String COLUM_SEXO = "Sexo";
+    private String COLUM_NACIMIENTO = "Nacimiento";
+}
---
+++ app/src/main/java/com/mrz/asstn/dbHelper.java
@@ -0,0 +1,6 @@
+package com.mrz.asstn;
+
+import android.database.sqlite.SQLiteDatabase;
+
+public class dbHelper extends SQLiteDatabase {
+}
