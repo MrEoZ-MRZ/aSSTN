d27412c3224d52a05bf72f7e3aadf27f24fe475f
MrEoZ <41388002+MrEoZ-MRZ@users.noreply.github.com>
2021-10-29 08:41:15 -0300

    Update MainActivity.java

--- app/src/main/java/com/mrz/asstn/MainActivity.java
+++ app/src/main/java/com/mrz/asstn/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Verificacion();
+        
     }
 
     boolean twice;
